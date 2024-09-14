409cc70205d4262499899d70e6babd13d2cf12c6
Yara Afifi <yaraafifi106@gmail.com>
2024-09-14 16:00:41 +0300

    Any Comment You like

---
+++ lib/bookly_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Doc App',
+        debugShowCheckedModeBanner: false,
+      )
+    );
+  }
+}
